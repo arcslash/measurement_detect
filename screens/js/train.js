94b2d158ce449e614b9ef2bcaf007096b8fdea46
Ishara Upamal <isharaux@gmail.com>
2018-10-01 14:21:17 +0530

    changes over the gui training interface

--- screens/js/train.js
+++ screens/js/train.js
@@ -8,8 +8,9 @@ var style_country = "";
 var stage;
 var points = ([]);
 var i = 0;
+var point_no = 0;
 const fs = require('fs');
-const point_name = ['a','b','c','d','e','f','g','h','i','j'];
+const point_name = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','u','p'];
 function init(){
     $('#train_image').css('height', $('#train_image').naturalHeight);
     $('#train_image').css('width', $('#train_image').naturalWidth); 
@@ -30,6 +31,8 @@ function show_inputs(){//after clicking new
 function activate_canvas(){//after submit
   $("#train_image").attr("src","http://localhost:5000/video_feed");
   $('#point-canvas').css('cursor', 'crosshair');
+  point_no = 0;
+
   
   
 
@@ -38,6 +41,7 @@ function activate_canvas(){//after submit
   stage.on("stagemousedown",function(event){
     console.log("clicked:" + event.stageX + "," + event.stageY);
     // point = new createjs.Shape();
+    
 
     var g = new createjs.Graphics();
     g.setStrokeStyle(1);
@@ -45,16 +49,27 @@ function activate_canvas(){//after submit
     g.beginFill(createjs.Graphics.getRGB(255,0,0));
     g.drawCircle(0,0,3);
     point = new createjs.Shape(g);
+
+    var text = new createjs.Text(point_name[point_no], "20px Arial", "#ff7700");
+    
+    text.x = event.stageX;
+    text.y = event.stageY;
+    text.textBaseline = "alphabetic";
+   
     
     // point.graphics.beginFill("blue").drawCircle(0, 0, 2);
     point.x = event.stageX;
     point.y = event.stageY;
+    
     console.log("[+]Image width:" + img.width);
     console.log("[+]Image Natural:" + img.naturalWidth);
     points.push([event.stageX / img.width * img.naturalWidth, event.stageY / img.height * img.naturalHeight]);
     console.log(points[0])
     stage.addChild(point);
+    stage.addChild(text);
     stage.update();
+    add_table(point_name[point_no], event.stageX, event.stageY);
+    point_no++;
 
 
   })
@@ -123,7 +138,13 @@ function start_train(){
 function add_measurement(){
   $('#measurement-table').append( '<tr><td><input type="text" name="measurement"/></td><td><input type="text" name="sp"/></td><td><input type="text" name="ep"/></td></tr>' );
 }
-
+function add_table(label, xcor, ycor){
+  $('#point-locations').append( '<tr>\
+  <th>'+label+'</th>\
+  <td>'+ xcor +'</td>\
+  <td>'+ ycor +'</td>\
+  </tr>');
+}
 
 
 
